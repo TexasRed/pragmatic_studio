2e51100e62a324d37febca5567235fd774523450
Kai Huang <kai.huang@appfolio.com>
2015-05-24 21:38:55 -0700

    I commit the changes to git repo.

---
+++ flix.rb
@@ -0,0 +1,1 @@
+puts 'mikey's favor
---
+++ studio_game.rb
@@ -0,0 +1,23 @@
+name = 'larry'
+
+name1 = 'larry'
+
+name2 = 'curly'
+
+name3 = 'moe'
+
+health = 60
+
+puts name + '\'s health is ' + health.to_s
+
+puts "#{name}'s health is #{health}"
+
+puts "#{name}'s health is #{health * 3}"
+
+puts "#{name}'s health is #{health / 9.0}"
+
+puts "#{name}'s health is #{health / 9}"
+
+puts "Players:\n\tlarry\n\tcurly\n\tmoe"
+
+puts "Players:\n\t#{name1}\n\t#{name2}\n\t#{name3}"
